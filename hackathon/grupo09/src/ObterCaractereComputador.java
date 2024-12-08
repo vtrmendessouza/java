b5ff3734dc9d634ee2c2ccd33ddf96e26d895364
vtrmendessouza <vtrmendessouza@gmail.com>
2024-12-08 18:42:32 -0300

    100%

--- hackathon/grupo09/src/ObterCaractereComputador.java
+++ hackathon/grupo09/src/ObterCaractereComputador.java
@@ -4,39 +4,22 @@ import java.util.Scanner;
 public class ObterCaractereComputador {
 
     // Define os caracteres aceitos
-    private static final char[] CARACTERES_IDENTIFICADORES_ACEITOS = {'X', 'O', 'A', 'B', 'C'};
+    final static String CARACTERES_IDENTIFICADORES_ACEITOS = "XO0UC";
 
     static char obterCaractereComputador(Scanner teclado, char caractereUsuario) {
         char caractereComputador;
 
         while (true) {
-            System.out.println("Digite o caractere para o computador (X, O, A, B, C):");
+            System.out.print("Digite o caractere para o computador (X, O, 0, U, C): ");
             caractereComputador = teclado.next().toUpperCase().charAt(0);
 
-            // Verifica se o caractere está na lista de aceitos e é diferente do caractere do usuário
-            if (caractereEhValido(caractereComputador) && caractereComputador != caractereUsuario) {
+            // Verifica se o caractere está na lista aceita e é diferente do caractere do usuário
+            if (CARACTERES_IDENTIFICADORES_ACEITOS.indexOf(caractereComputador) != -1 && caractereComputador != caractereUsuario) {
                 break; // Sai do loop se for válido
             } else {
                 System.out.println("Caractere inválido ou já escolhido pelo usuário. Tente novamente.");
             }
         }
-    return caractereComputador;
-    }
-    private static boolean caractereEhValido(char caractere) {
-        for (char c : CARACTERES_IDENTIFICADORES_ACEITOS) {
-            if (c == caractere) {
-                return true;
-            }
-        }
-    return false;
+        return caractereComputador;
     }
 }
-/*
-funciona assim :
-Constante de caracteres aceitos: CARACTERES_IDENTIFICADORES_ACEITOS define quais caracteres podem ser escolhidos.
-Loop de validação:
-Solicita ao usuário que insira um caractere para o computador.
-Verifica se o caractere está na lista de caracteres aceitos e se é diferente do caractere já escolhido pelo usuário.
-Método auxiliar: caractereEhValido verifica se o caractere pertence à lista de caracteres permitidos.
-Saída: O caractere válido é retornado e exibido.
-*/
---
+++ hackathon/grupo09/src/SortearValorBooleano.java
@@ -0,0 +1,10 @@
+//Nathan Vinicius
+import java.util.Random;
+
+public class SortearValorBooleano {
+
+    static boolean sortearValorBooleano() {
+        Random random = new Random();
+        return random.nextBoolean();
+    }
+}
