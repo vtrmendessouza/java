8047996565fb4baadc27f3e550b33e0979bf4791
vtrmendessouza <vtrmendessouza@gmail.com>
2025-01-07 21:20:30 -0300

    contatos v2

---
+++ doc16/src/Pessoa.java
@@ -0,0 +1,69 @@
+import org.apache.commons.lang3.StringUtils;
+import java.util.ArrayList;
+
+public class Pessoa {
+    public static int contador;
+    private int id;
+    private String nome;
+    private ArrayList <Telefone> telefones = new ArrayList();
+    private String email;
+
+    //construtor da classe pessoa
+    public Pessoa(String nome,  ArrayList telefones, String email) {
+        contador++;
+        this.id = contador;
+        setNome(nome);
+        this.telefones = telefones;
+        setEmail(email);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        //aqui implementariamos as validações necessárias
+        //antes de inserir o nome  
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setTelefone(String ddd, String telefone) {
+        //aqui implementariamos as validações necessárias
+        //antes de inserir o telefone 
+        telefones.add(new Telefone(ddd, telefone));
+    }
+
+    public ArrayList getTelefones() {
+        return telefones;
+    }
+
+    public void setEmail(String email) {
+        //aqui implementariamos as validações necessárias
+        //antes de inserir o email 
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    //cria o método to string utilizado para converter o objeto para string
+    //quando for necessário imprimir os dados do objeto na tela por outra parte do nosso
+    //programa
+    @Override
+    public String toString() {
+        String strTelefones = "";
+        
+        if (!telefones.isEmpty()) {
+            for (Telefone telefone : telefones) {
+                strTelefones += telefone.toString() + ", ";
+            }
+        }
+
+        return "ID: " + id + ", Nome: " + nome + ", Telefones: " +  StringUtils.removeEnd(strTelefones, ", ") + ", Email: " + email;
+    }
+}
---
+++ doc16/src/Telefone.java
@@ -0,0 +1,33 @@
+public class Telefone {
+
+    public enum Tipo {
+        CELULAR, FIXO;
+    }
+    private String ddd;
+    private String numero;
+    private Tipo tipo;
+
+    public Telefone(String ddd, String numero, Telefone.Tipo tipo) {
+        this.ddd = ddd;
+        this.numero = numero;
+        this.tipo = tipo;
+    }
+    public String getDdd() {
+        return ddd;
+    }
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public Tipo getTipo() {
+        return tipo;
+    }
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+}
